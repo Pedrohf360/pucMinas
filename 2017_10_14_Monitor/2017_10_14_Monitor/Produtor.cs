3b1a68b595e997d8eac52f1c22c813a83540ac02
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2017-11-07 12:35:21 -0200

    Revert "Revert "Sincronização por Semáforo (API)""
    This reverts commit 385957e67b034a875e72b387045a3aa3477dd6ec.

---
+++ 2017_10_14_Monitor/2017_10_14_Monitor/Produtor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _2017_10_14_Monitor
+{
+    // Adiciona itens ao buffer. Se o buffer está cheio, deve esperar!
+    class Produtor
+    {
+        string nome;
+        string matricula;
+        static int cont;
+
+        Random randomTime;
+
+        Buffer buf;
+
+        public static  int Cont { get {return cont; }set {cont = value; }}
+        public string Nome { get {return nome;} set {nome = value; }}
+        public string Matricula { get { return matricula; } set { matricula = value; } }
+
+        static Produtor()
+        {
+            cont = 0;
+        }
+
+        public Produtor(Buffer b, Random r, string nome, string matricula)
+        {
+            this.buf = b;
+            this.nome = nome;
+            this.matricula = matricula;
+            this.randomTime = r;
+        }
+     
+        public void Produzir()
+        {
+            string texto = this.nome + " " + this.matricula;
+
+            cont = texto.Length;
+
+            for (int i = 0; i < cont; i++)
+            {
+                Thread.Sleep(randomTime.Next(1, 200));
+                buf.Caractere = texto[i];
+
+                Buffer.SemaphCons.Release();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n" + Thread.CurrentThread.Name +
+                " terminou de produzir.\nTerminando " + Thread.CurrentThread.Name + ".\n");
+            Console.ResetColor();
+        }
+
+        }
+    }
+
