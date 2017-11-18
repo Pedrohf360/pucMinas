a4931f427894e0721efdfcdeefb59fab26fc5325
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2017-11-18 21:28:09 -0200

    Método "PauseThreads" implementado.
    Utilizando "Suspend()" e "Resume()".

--- Sincronizacao (Semaforo e Monitor)/Semaforo.cs
+++ Sincronizacao (Semaforo e Monitor)/Semaforo.cs
@@ -9,13 +9,66 @@ namespace _2017_11_07_Semaforo
 {
     class Program
     {
+        static Thread[] consumThreads = new Thread[4];
+        static Thread[] prodThreads = new Thread[4];
+        static Thread pauseThread;
+        static int i = 0;
+        static bool pause;
+
+        static void PauseThreads()
+        {
+            ConsoleKeyInfo k = new ConsoleKeyInfo();
+
+            k = Console.ReadKey(true);
+
+            if (pause)
+            {
+                consumThreads[i].Resume();
+                prodThreads[i].Resume();
+
+                pause = false;
+
+                pauseThread = new Thread(new ThreadStart(PauseThreads));
+                pauseThread.Start();
+            }
+            else if (k.Key == ConsoleKey.T)
+            {
+                consumThreads[i].Suspend();
+
+                prodThreads[i].Suspend();
+
+                pause = true;
+
+                pauseThread = new Thread(new ThreadStart(PauseThreads));
+                pauseThread.Start();
+
+                Console.WriteLine("\nPAUSADO...\nPara continuar, pressione qualquer tecla.");
+            }
+            
+        }
+
+        static void ContagemRegressiva(int quantSegundos, string textoContagem)
+        {
+            Console.Write(textoContagem+ " ");
+            for (int i = quantSegundos; i > 0; i--)
+            {
+                Console.Write(i + ", ");
+
+                Thread.Sleep(1000);
+            }
+        }
+
         static void Main(string[] args)
         {
+            int quantSegPause;
+
+            consumThreads = new Thread[3];
+            prodThreads = new Thread[3];
+
             Console.WriteLine("\t------------Monitor - MultiThreads------------");
             Console.WriteLine("\nGRUPO:\tNOME:\t\t\tMATRICULA:" +
                             "\n\tPedro Henrique\t\t580544" +
                             "\n\tLucas Gomes\t\t578927" +
-                            "\n\tHenrique Kirschke\t573948" +
                             "\n\tItalo Fabricio\t\t573962\n");
 
             ConsoleKeyInfo k = new ConsoleKeyInfo();
@@ -24,71 +77,70 @@ namespace _2017_11_07_Semaforo
 
             Random random = new Random();
 
-            Produtor[] prod = new Produtor[4];
+            pauseThread = new Thread(new ThreadStart(PauseThreads));
+
+            Produtor[] prod = new Produtor[3];
 
             prod[0] = new Produtor(buffer, random, "Pedro H. - ", "580544");
             prod[1] = new Produtor(buffer, random, "Lucas G. - ", "578927");
-            prod[2] = new Produtor(buffer, random, "Henrique K. - ", "573948");
-            prod[3] = new Produtor(buffer, random, "Italo F. - ", "573962");
+            prod[2] = new Produtor(buffer, random, "Italo F. - ", "573962");
 
-            Consumidor[] consum = new Consumidor[4];
+            Consumidor[] consum = new Consumidor[3];
 
             consum[0] = new Consumidor(buffer, random);
             consum[1] = new Consumidor(buffer, random);
             consum[2] = new Consumidor(buffer, random);
-            consum[3] = new Consumidor(buffer, random);
 
-            Thread[] prodThreads = new Thread[4];
+            
             prodThreads[0] = new Thread(new ThreadStart(prod[0].Produzir));
             prodThreads[1] = new Thread(new ThreadStart(prod[1].Produzir));
             prodThreads[2] = new Thread(new ThreadStart(prod[2].Produzir));
-            prodThreads[3] = new Thread(new ThreadStart(prod[3].Produzir));
 
             prodThreads[0].Name = "Thread producao 1 ";
             prodThreads[1].Name = "Thread producao 2";
             prodThreads[2].Name = "Thread producao 3";
-            prodThreads[3].Name = "Thread producao 4";
 
-            Thread[] consumThreads = new Thread[4];
             consumThreads[0] = new Thread(new ThreadStart(consum[0].Consumir));
             consumThreads[1] = new Thread(new ThreadStart(consum[1].Consumir));
             consumThreads[2] = new Thread(new ThreadStart(consum[2].Consumir));
-            consumThreads[3] = new Thread(new ThreadStart(consum[3].Consumir));
 
             consumThreads[0].Name = "Thread consumo 1 ";
             consumThreads[1].Name = "Thread consumo 2 ";
             consumThreads[2].Name = "Thread consumo 3 ";
-            consumThreads[3].Name = "Thread consumo 4 ";
 
-            for (int i = 0; i < prod.Length; i++)
+            Console.WriteLine("\n\t\t\t(T) = PAUSE/CONTINUE\n\n");
+            
+            quantSegPause = 5;
+            ContagemRegressiva(quantSegPause, "Iniciando em");
+
+            Console.WriteLine("\n" + new string('-', 50));
+
+            prodThreads[0].Start();
+            pauseThread.Start();
+
+            while (i < prod.Length)
             {
-
-                prodThreads[i].Start();
-
+                if (i == 0)
                 consumThreads[i].Start();
-
-                Thread.Sleep(5000);
-
-                if (i != prod.Length - 1)
+                else
                 {
-                    Console.WriteLine("\nDeseja pausar? (T)");
-
-                    k = Console.ReadKey();
-
-                    while (k.Key == ConsoleKey.T)
-                    {
-                        Console.Clear();
-
-                        Console.WriteLine("\nDeseja continuar? (C)");
-
-                        k = Console.ReadKey();
-                    }
+                    prodThreads[i].Start();
+                    consumThreads[i].Start();
                 }
+
+                consumThreads[i].Join();
+                prodThreads[i].Join();
+
                 Produtor.Cont = 0;
+                i++;
+
+                if (i < prod.Length)
+                ContagemRegressiva(5, "Continua em");
+				else pauseThread.Abort();
             }
 
             Console.WriteLine("\n\nPressione qualquer tecla para sair.");
-            Console.ReadKey();
+            Console.ReadKey(true);
         }
     }
 
@@ -128,7 +180,7 @@ namespace _2017_11_07_Semaforo
 
             for (int i = 0; i < cont; i++)
             {
-                Thread.Sleep(randomTime.Next(1, 200));
+                Thread.Sleep(randomTime.Next(200, 500));
                 buf.Caractere = texto[i];
 
                 Buffer.SemaphCons.Release();
@@ -165,7 +217,7 @@ namespace _2017_11_07_Semaforo
 
             for (int i = 0; i < Produtor.Cont; i++)
             {
-                Thread.Sleep((randomTime.Next(1, 200)));
+                Thread.Sleep(randomTime.Next(200, 500));
                 this.dadosConsumidos += buf.Caractere;
 
                 Buffer.SemaphProd.Release();
